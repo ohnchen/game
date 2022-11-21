12db5ce013c5c8683697ba266085942e3fd25fcb
Jona Ohme <ohmejona@gmail.com>
2022-11-22 00:08:11 +0100

    implemented dragging of an element

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
-use sdl2::render::{WindowCanvas, Texture};
+use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
-// "self" imports the "image" module itself as well as everything else we listed
-use sdl2::image::{self, LoadTexture, InitFlag};
+use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
 
 fn render(
@@ -13,17 +12,28 @@ fn render(
     position: Point,
     sprite: Rect,
 ) -> Result<(), String> {
-
     canvas.clear();
-    let (width, height) = canvas.output_size()?;
 
-    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    let screen_rect = Rect::from_center(position, sprite.width(), sprite.height());
 
     canvas.copy(texture, sprite, screen_rect)?;
     canvas.present();
 
     Ok(())
+}
+
+fn match_mouse_pos(
+    mouse_pos_x: i32,
+    mouse_pos_y: i32,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+) -> bool {
+    mouse_pos_x > x - height / 2
+        && mouse_pos_x < x + height / 2
+        && mouse_pos_y > y - width / 2
+        && mouse_pos_y < y + width / 2
 }
 
 fn main() -> Result<(), String> {
@@ -32,37 +42,67 @@ fn main() -> Result<(), String> {
 
     let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("game tutorial", 800, 600)
+    let window = video_subsystem
+        .window("game", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
 
-    let mut canvas = window.into_canvas().build()
+    let mut canvas = window
+        .into_canvas()
+        .build()
         .expect("could not make a canvas");
 
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/mail.png")?;
-    let position = Point::new(0, 0);
 
-    let sprite = Rect::new(0, 0, 32, 32);
+    let (width, height) = canvas.output_size()?;
+    let mut position = Point::new(width as i32 / 2, height as i32 / 2);
+
+    let sprite = Rect::new(0, 0, 64, 64);
 
     canvas.set_draw_color(Color::RGB(42, 53, 77));
     canvas.clear();
 
     let mut event_pump = sdl_context.event_pump()?;
+    let mut hit = false;
     'running: loop {
+        let mouse_pos_x = event_pump.mouse_state().x();
+        let mouse_pos_y = event_pump.mouse_state().y();
+
         // Handle events
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'running;
-                },
+                }
+                Event::MouseButtonDown {
+                    mouse_btn: sdl2::mouse::MouseButton::Left,
+                    ..
+                } => {
+                    if match_mouse_pos(mouse_pos_x, mouse_pos_y, position.x(), position.y(), 64, 64)
+                    {
+                        hit = true;
+                    }
+                }
+                Event::MouseButtonUp {
+                    mouse_btn: sdl2::mouse::MouseButton::Left,
+                    ..
+                } => {
+                    hit = false;
+                }
                 _ => {}
             }
         }
 
-        // Render
+        if hit {
+            position = Point::new(mouse_pos_x, mouse_pos_y);
+        }
+
         render(&mut canvas, &texture, position, sprite)?;
 
         // Time management!
